80b3e1389c6b1a3384fdafc02c8c64bc6d9a2dec
Jinn Foxx <foxx.jinn@gmail.com>
2020-12-18 20:43:12 -0600

    spinnyButton accepts html link element, saves href, adds listener, clones link upon click

--- source/spinnyButton.js
+++ source/spinnyButton.js
@@ -1,9 +1,49 @@
 ;(function(){
-    function spinnyButton(link) {
-        if (typeof link === 'a') {
+    
+    // const SpinButton = function(href, animationTime, revolutions) {
+    //     this.href = href
+    //     this.animationTime = animationTime
+    //     this.revolutions = revolutions 
+    // }
+
+            /*
+                Listen for click
+                If Clicked, Start Animation
+                When Animation ends, load href
+            */
+
+    function spinnyButton(link, animationTime = 1, revolutions = 1) {
+        console.log(link.tagName)
+        if (link.tagName === 'A') {   
+            const href = link.href
+            console.log(href)
+
+            link.href = ''
+
+            link.addEventListener('click', (e)=> {
+                const copyLink = link.cloneNode(true)
+                copyLink.style.pointerEvents = 'none'
+                document.querySelector('body').appendChild(copyLink)
+                e.preventDefault()
+            })
+
             
+
         } else {
             throw new TypeError('SpinnyButton only accepts HTML link')
         }
     }
+
+    if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+
+        // AMD. Register as an anonymous module.
+        define(function() {
+          return spinnyButton;
+        });
+      } else if (typeof module !== 'undefined' && module.exports) {
+            module.exports = spinnyButton;
+            module.exports.spinnyButton = spinnyButton;
+      } else {
+            window.spinnyButton = spinnyButton;
+      }
 }());
