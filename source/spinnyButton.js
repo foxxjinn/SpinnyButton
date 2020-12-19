f1ed05d3252b46aaf708c32f2cd0c0c680e0f36c
Jinn Foxx <foxx.jinn@gmail.com>
2020-12-18 19:40:16 -0600

    initial commit - example & source files created

---
+++ source/spinnyButton.js
@@ -0,0 +1,9 @@
+;(function(){
+    function spinnyButton(link) {
+        if (typeof link === 'a') {
+            
+        } else {
+            throw new TypeError('SpinnyButton only accepts HTML link')
+        }
+    }
+}());
